285fbc670239940b693b4e03cbd621a341f34899
shahmadi <ahmadi.shayan64@gmail.com>
2018-04-18 14:38:40 -0400

    basic functionality with three tasks

--- todo.js
+++ todo.js
@@ -1,1 +1,13 @@
 // alert("Connected")
+
+$("li").click(function(){
+	$(this).toggleClass("completed");
+})
+
+$("span").click(function(event){
+	// alert("clicked!");
+	$(this).parent().fadeOut(500,function(){
+		$(this).remove();
+	});
+	event.stopPropagation();
+})
