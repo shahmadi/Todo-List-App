b81573bb4cdc938c218b136e216034d001bdc7e7
shahmadi <ahmadi.shayan64@gmail.com>
2018-04-20 09:07:15 -0400

    functionality completion

--- todo.js
+++ todo.js
@@ -1,13 +1,21 @@
 // alert("Connected")
 
-$("li").click(function(){
+$("ul").on("click","li",function(){
 	$(this).toggleClass("completed");
 })
 
-$("span").click(function(event){
+$("ul").on("click","span",function(event){
 	// alert("clicked!");
 	$(this).parent().fadeOut(500,function(){
 		$(this).remove();
 	});
 	event.stopPropagation();
+})
+
+$("input[type='text']").keypress(function(event){
+	if(event.which===13){
+		var newtask=$(this).val();
+		$(this).val("");
+		$("ul").append("<li><span>X </span>"+newtask+"</li>");
+	}
 })
